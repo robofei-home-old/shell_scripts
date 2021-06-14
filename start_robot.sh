ac4fa09fe27ded747160281ab25e9e570fff7de0
Khaled Hazime <hazimekhaled01@gmail.com>
2021-06-14 14:39:26 -0300

    Added kinect variable

--- start_robot.sh
+++ start_robot.sh
@@ -44,10 +44,10 @@ enable_talk=true
 enable_map=true
 enable_people=false
 enable_navigation=true
-enable_manipulator=false
+enable_manipulator=true
 enable_objects=false
 enable_head=false
-
+enable_kinect=false
 # start robot
 roslaunch hera bring_up.launch enable_rviz:=$enable_rviz map_name:=$map_name resources:="$resources" robot_name:=$robot_name robot_model:="$robot_model" teleop_keyboard:=$teleop_keyboard init_pos_x:=$init_pos_x init_pos_y:=$init_pos_y init_pos_z:=$init_pos_z init_yaw:=$init_yaw enable_hear:=$enable_hear enable_talk:=$enable_talk enable_map:=$enable_map enable_people:=$enable_people enable_navigation:=$enable_navigation enable_manipulator:=$enable_manipulator enable_objects:=$enable_objects enable_head:=$enable_head
 #roslaunch hera bring_up.launch robot_model:="$robot_model"
